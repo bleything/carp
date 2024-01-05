2c99f997517ae12211cd448e8d6914bed81f8761
Ben Bleything <ben@bleything.net>
2024-01-04 23:32:32 -0800

    set up oled featherwing

---
+++ src/display.h
@@ -0,0 +1,2 @@
+void clear_screen();
+void reset_screen();
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,49 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SH110X.h>
+
+#include "display.h"
+
+#define BUTTON_DISPLAY_A 9
+#define BUTTON_DISPLAY_B 8
+#define BUTTON_DISPLAY_C 7
+
+Adafruit_SH1107 display = Adafruit_SH1107(64, 128, &Wire);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  delay(250); // wait for the OLED to power up
+  display.begin(0x3C, true); // Address 0x3C default
+  display.setRotation(1);
+
+  // clear the display
+  display.clearDisplay();
+  display.display();
+
+  // configure display buttons
+  pinMode(BUTTON_DISPLAY_A, INPUT_PULLUP);
+  pinMode(BUTTON_DISPLAY_B, INPUT_PULLUP);
+  pinMode(BUTTON_DISPLAY_C, INPUT_PULLUP);
+
+  reset_screen();
+}
+
+void clear_screen() {
+  display.clearDisplay();
+  display.setCursor(0, 0);
+}
+
+void reset_screen() {
+  display.setTextSize(1);
+  display.setTextColor(SH110X_WHITE);
+
+  clear_screen();
+  display.display();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  clear_screen();
+  display.display();
 }
